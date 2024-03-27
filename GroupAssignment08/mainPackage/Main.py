419fc98fb36194c1850269868b3a6971b8e2a080
binta <binta@Binta.wifi.uc.edu>
2024-03-27 15:42:47 -0400

    new main commit

--- GroupAssignment08/mainPackage/Main.py
+++ GroupAssignment08/mainPackage/Main.py
@@ -1,3 +1,13 @@
+# Name: Binta Drammeh
+# email: Drammeba@mail.uc.edu
+# Assignment Number: Assignment 08
+# Due Date: March 28, 2024
+# Course/Section: IS 4030 - 002
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: Extract data from SQL Server 
+#Citations:
+# Anything else that's relevant:
+
 import pyodbc
 from groceryPackage.grocery import groceryData
 from sqlPackage.sql import sqlQuery
